dd608d55cda145f9f8042c91eb139e5434f9c1f5
MA_ <73825133+MichelangeloAloisio@users.noreply.github.com>
2024-06-05 12:32:59 +0200

    Update Deseq2_DEG.R

--- Deseq2_DEG.R
+++ Deseq2_DEG.R
@@ -1,9 +1,10 @@
 ### Here is describe the pipeline to find the Differential Expressed Genes (DEGs)
 
 #### STEP1_ upload and formatting counts dataframe
-data <- read.table("D:/feature_counts_subread.counts.txt", header=T, row.names=1) ### upload raw counts
+data <- read.table("feature_counts_subread.counts.txt", header=T, row.names=1) ### upload raw counts
 meta <- read.table("decoder_GSE146853_Healthy_vs_Diarrhea.txt", header=T, row.names=1) ### upload metadata
 meta <- meta[order(meta$group.ID), ] ### Sort the DataFrame meta based on the column group.ID
 
 all(colnames(data) %in% rownames(meta)) ## this command checks that the column names of the data and meta dataframes are the same
 all(colnames(data) == rownames(meta)) # Check if each column name in 'data' exactly matches a row name in 'meta'
+
