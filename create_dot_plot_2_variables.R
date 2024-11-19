aeeff5b0fae46523bbb266d48f7a5e5fd104a69f
MA_ <73825133+MichelangeloAloisio@users.noreply.github.com>
2024-11-19 21:47:26 +0100

    Update create_dot_plot_2_variables.R

--- create_dot_plot_2_variables.R
+++ create_dot_plot_2_variables.R
@@ -1,21 +1,19 @@
 
-# Assicurati che ggplot2 sia caricato
+# Carica i pacchetti necessari
 library(ggplot2)
+library(dplyr)
 
-
-# Sostituisci "path/to/your/file.csv" con il percorso reale del tuo file CSV
+# Leggi il file CSV
 dati <- read.csv("D:/Desktop/progetti_v1/15_RNA_SEQ_IBS_FATIMA/08_rsiposte_revisori/Figure_/dotplot_reactome_/reactome_54_genes.csv", sep = ";")
 
-# Verifica che i dati siano stati letti correttamente
-class(dati)
+# Ordina i dati in base a Curated_Total in ordine decrescente
+dati <- dati %>%
+  arrange(desc(Curated_Total))
 
-
-ggplot(dati, aes(x = Curated_Total, y = Pathway_name, color = Curated_found, size = Curated_Total)) +
-  geom_point(alpha = 0.6) + # Aggiungi trasparenza per migliorare la leggibilità
-  scale_color_gradient(low = "blue", high = "red") + # Usa un gradiente di colori dal blu al rosso
-  scale_size(range = c(1, 10)) + # Regola la gamma di dimensioni dei cerchi
-  labs(x = 'Curated Total', y = 'Pathway Name', color = 'Curated Found', size = 'Curated Total\nSize') + # Aggiorna le etichette
-  theme(axis.title = element_text(face = 'bold'), axis.text = element_text(face = 'bold')) # Modifica il tema
-
-
-
+# Crea il grafico
+ggplot(dati, aes(x = Curated_Total, y = reorder(Pathway_name, Curated_Total), color = Curated_found, size = Curated_Total)) +
+  geom_point(alpha = 0.6) +  # Aggiungi trasparenza per migliorare la leggibilità
+  scale_color_gradient(low = "blue", high = "red") +  # Gradiente di colori dal blu al rosso
+  scale_size(range = c(1, 10)) +  # Regola la gamma di dimensioni dei cerchi
+  labs(x = 'Curated Total', y = 'Pathway Name', color = 'Curated Found', size = 'Curated Total\nSize') +  # Etichette
+  theme(axis.title = element_text(face = 'bold'), axis.text = element_text(face = 'bold'))  # Tema personalizzato
